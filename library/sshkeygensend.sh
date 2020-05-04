06ca9b847971b55fefbe1009c5f8f8f45b4de0b2
CHOI GOOK HYUN <bluehelix@gmail.com>
2020-05-04 16:58:16 +0900

    stdout stderr redirect to /dev/null

--- library/sshkeygensend.sh
+++ library/sshkeygensend.sh
@@ -2,17 +2,17 @@ declare -a servers
 servers=$(awk '{ print $1 }' /etc/hosts | grep ^192.168.122)
 
 function sendkeys(){
-  for ipadr in ${servers[*]}; do sshpass -p openstack ssh-copy-id -o PubkeyAuthentication=no -o StrictHostKeyChecking=no root@${ipadr} ; done
+  for ipadr in ${servers[*]}; do sshpass -p openstack ssh-copy-id -o PubkeyAuthentication=no -o StrictHostKeyChecking=no root@${ipadr} > /dev/null 2>&1 ; done
 exit
 }
 
 function genkeys(){
-  ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
+  ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa > /dev/null 2>&1 
 }
 
 if [ -f ~/.ssh/id_rsa ]; then
   echo "the file exist. passed it"
-  sendkeys
+  sendkeys 
 else
   genkeys
   sendkeys
