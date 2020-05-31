4ddf54279eb9e214de82c4dd0691b88fb41425f6
tang <tang@newws.dustbox.kr>
2020-05-31 21:30:17 +0900

    update script

--- library/sshkeygensend.sh
+++ library/sshkeygensend.sh
@@ -1,18 +1,20 @@
 case "$1" in
   openstack)
     server=$(awk '{ print $2 }' /etc/hosts | grep ^node)
+    password=openstack
     ;;
   kubernetes)
     server=$(awk '{ print $1 }' /etc/hosts | grep ^192.168.122)
+    password=kubernetes
     ;;
   *)
-    echo "Using: $0 {openstack|kubernetes}"
+    echo "Using: $1 {openstack|kubernetes}"
     exit 1
 esac
 
 
 function sendkeys(){
-  for ipadr in ${server} ; do sshpass -p openstack ssh-copy-id -o PubkeyAuthentication=no -o StrictHostKeyChecking=no root@${ipadr} > /dev/null 2>&1 ; done
+  for ipadr in ${server} ; do sshpass -p ${password} ssh-copy-id -o PubkeyAuthentication=no -o StrictHostKeyChecking=no root@${ipadr} > /dev/null 2>&1 ; done
 exit
 }
 
